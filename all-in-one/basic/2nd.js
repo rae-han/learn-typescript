67a449a9b11f7c64aeb37ad0046eca7e88c6e940
raehan <raehan900809@gmail.com>
2022-08-22 22:56:22 +0900

    add: enum, object type

---
+++ all-in-one/basic/2nd.js
@@ -0,0 +1,11 @@
+// const a: string = 'hello';
+// const b: String = 'hell';
+// // string과 String의 타입은 다르다.
+//
+// type World = "world";
+// const c: World = 'world'; // control + space를 하면 자동 완선 추천해준다.
+// const d = `hello ${c}`;
+//
+// type Greeting = `hello ${World}`; // 이렇게도 가능하다. 타입이 hello world도 hello hell도 될수 있다.
+var aa = 3 /* EDirection.Up */;
+var ab = 4 /* EDirection.Down */;
