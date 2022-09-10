57c539039aaaa96dce498a363577e56f838e8638
raehan <raehan900809@gmail.com>
2022-09-10 23:58:27 +0900

    modify: variable name

--- all-in-one/basic/1st.ts
+++ all-in-one/basic/1st.ts
@@ -13,19 +13,19 @@
 // console.log(ee);
 
 //
-//
-// // function add(x: number, y: number): number/* 리턴 값도 숫자 */ { return x + y }
-// //
-// // const add: (x: number, y: number) => number = (x, y) => x + y;
-// // // ===
-// // type Add = (x: number, y: number) => number; // 타입 애일리어스(type alias)
-// // const add: Add = (x, y) => x + y;
-// // // ===
-// // interface Add {
-// //   (x: number, y: number): number;
-// // }
-// // const add: Add = (x, y) => x + y;
-//
+
+function add1(x: number, y: number): number/* 리턴 값도 숫자 */ { return x + y }
+
+const add2: (x: number, y: number) => number = (x, y) => x + y;
+// ===
+type Add3 = (x: number, y: number) => number; // 타입 애일리어스(type alias)
+const add3: Add3 = (x, y) => x + y;
+// ===
+interface Add4 {
+  (x: number, y: number): number;
+}
+const add4: Add4 = (x, y) => x + y;
+
 //
 // const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 };
 //
