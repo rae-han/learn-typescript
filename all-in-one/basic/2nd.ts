a13e059a135f496fe38df1545455d0ab920b66ad
raehan <raehan900809@gmail.com>
2022-09-19 21:19:27 +0900

    add: K-V typeof keyof

--- all-in-one/basic/2nd.ts
+++ all-in-one/basic/2nd.ts
@@ -85,20 +85,21 @@ type OD = keyof typeof ODirection;
 
 //
 // // 잠깐 keyof는?
-const object = { a: 1, b: 2, c: 3 };
+const object1 = { a: 1, b: 2, c: 3 };
 // // 위 객체에서 key만 가져오고 싶으면? keyof
-// // type Key = keyof obj3; // obj3은 값인데 타입으로 사용하려니 안된다?
-// type Key = keyof typeof obj3; // 값을 타입으로 사용하고 싶다면 typeof
+
+// // type Key = keyof object; // obj3은 값인데 타입으로 사용하려니 안된다?
+// type Key = keyof typeof object; // 값을 타입으로 사용하고 싶다면 typeof
 // // 뽑아낸 타입을 Key라는 타입으로 만든다.
 // // 즉 자바스크립트 값인 obj3은 타입으로 쓸수 없어서
 // // 타입으로 쓰기 위해 typeof을 붙여주고
 // // keyof로 abc를 뽑아내고
 // // Key라는 타입으로 만들었다.
 //
-// const obj4 = { a: 1, b: 2, c: 3 } as const;
+const object2 = { a: 1, b: 2, c: 3 } as const;
 //
-// // type Key2 = typeof obj3[keyof typeof obj4];
-// type Key2 = typeof obj4[keyof typeof obj4];
-// // as const를 사용하는 이유는 널널하게 타입을 뽑아내기 때문(number)
-// // 타입이 123이 안되고 number number number가 된다.
-// // 위 코드는 벨류들의 값을 타입으로 가져오고 싶다는 뜻.
+type Key1 = typeof object1[keyof typeof object2];
+type Key2 = typeof object2[keyof typeof object2];
+// as const를 사용하는 이유는 널널하게 타입을 뽑아내기 때문(number)
+// 타입이 123이 안되고 number number number가 된다.
+// 위 코드는 벨류들의 값을 타입으로 가져오고 싶다는 뜻.
