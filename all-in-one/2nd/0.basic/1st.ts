0358ffe22092a0cb72ca2387874507935fc9b4f8
raehan <raehan900809@gmail.com>
2023-01-04 21:15:50 +0900

    add: type add: template literal type

---
+++ all-in-one/2nd/0.basic/1st.ts
@@ -0,0 +1,11 @@
+// 함수 타입을 정의하는 방법
+type TAdd = (x: number, y: number) => number;
+interface IAdd {
+  (x: number, y: number): number
+}
+// function FAdd (x: number, y: number): number;
+// function FAdd(x, y) {
+//   return x+y
+// }
+
+export {}
---
+++ all-in-one/2nd/0.basic/2nd.ts
@@ -0,0 +1,12 @@
+// 템플릿 리터럴 타입
+type World = "world" | "hell";
+const a: World = "world";
+
+const b = `hello ${a}`;
+
+type Greeting = `hello ${World}`;
+
+const c: Greeting = 'hello world';
+const d: Greeting = 'hello hell';
+
+export {}
