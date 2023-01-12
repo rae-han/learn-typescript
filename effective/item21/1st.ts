7508e7b19d79720729ac7ca8102cdc8ee799f5d6
raehan <raehan900809@gmail.com>
2023-01-12 16:39:26 +0900

    add: Understand Type Wideninag Understand Type Narrowing

---
+++ effective/item21/1st.ts
@@ -0,0 +1,18 @@
+interface Vector3 {
+  x: number,
+  y: number,
+  z: number,
+}
+function getComponent(vector: Vector3, axis: 'x' | 'y' | 'z') {
+  return vector[axis];
+}
+
+let x = 'x';
+let vec = { x: 10, y: 20, z: 30 };
+
+// getComponent(vec, x);
+// TS2345: Argument of type 'string' is not assignable to parameter of type '"x" | "y" | "z"'.
+
+const mixed = ['x', 1]
+
+export {}
