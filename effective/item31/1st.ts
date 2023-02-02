c156d2a93a896811d79e4e7c19a75174ef96e590
raehan <raehan900809@gmail.com>
2023-02-02 14:49:27 +0900

    add: item 31: null values to perimeter

--- effective/item31/1st.ts
+++ effective/item31/1st.ts
@@ -1,21 +1,21 @@
-function extent(nums: number[]) {
-  let min, max;
-
-  for (const num of nums) {
-    if (!min) {
-      min = num;
-      max = num;
-    } else {
-      min = Math.min(min, num);
-      max = Math.max(max, num);
-      // Argument of type 'number | undefined' is not assignable to parameter of type 'number'.
-      // Type 'undefined' is not assignable to type 'number'.
-    }
-  }
-
-  return [min, max];
-}
-
-extent([1, 2, 6, 7, 8, 9]);
-
-export {};
+// function extent(nums: number[]) {
+//   let min, max;
+//
+//   for (const num of nums) {
+//     if (!min) {
+//       min = num;
+//       max = num;
+//     } else {
+//       min = Math.min(min, num);
+//       max = Math.max(max, num);
+//       // Argument of type 'number | undefined' is not assignable to parameter of type 'number'.
+//       // Type 'undefined' is not assignable to type 'number'.
+//     }
+//   }
+//
+//   return [min, max];
+// }
+//
+// const [min, max] = extent([1, 2, 6, 7, 8, 9]);
+//
+// export {};
