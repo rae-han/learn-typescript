7139abb6f7f9a191779db017b9d1b788d7756014
raehan <raehan900809@gmail.com>
2023-02-09 13:23:32 +0900

    add: item38: narrowest any

--- effective/item38/4th.ts
+++ effective/item38/4th.ts
@@ -1,7 +1,40 @@
-interface Foo { foo: string; }
-interface Bar { bar: string; }
+// interface Foo {
+//   foo: string;
+// }
+// interface Bar {
+//   bar: string;
+// }
+// declare function expressionReturningFoo(): Foo;
+// function processBar(b: Bar) {
+//   /* ... */
+// }
+// interface Config {
+//   a: number;
+//   b: number;
+//   c: {
+//     key: Foo;
+//   };
+// }
+// declare const value: Bar;
+// const config: Config = {
+//   a: 1,
+//   b: 2,
+//   c: {
+//     // key: value
+//     // Property 'foo' is missing in type 'Bar' but required in type 'Foo'.
+//   },
+// };
+
+interface Foo {
+  foo: string;
+}
+interface Bar {
+  bar: string;
+}
 declare function expressionReturningFoo(): Foo;
-function processBar(b: Bar) { /* ... */ }
+function processBar(b: Bar) {
+  /* ... */
+}
 interface Config {
   a: number;
   b: number;
@@ -12,11 +45,10 @@ interface Config {
 declare const value: Bar;
 const config: Config = {
   a: 1,
-  b: 2,
+  b: 2, // These properties are still checked
   c: {
-    key: value
-    // Property 'foo' is missing in type 'Bar' but required in type 'Foo'.
-  }
+    key: value as any,
+  },
 };
 
-export {}
+export {};
