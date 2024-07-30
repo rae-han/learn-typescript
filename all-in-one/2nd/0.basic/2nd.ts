e12f451ec44aef26c80daf64a19e983b7f72df42
raehan <raehan900809@gmail.com>
2024-07-30 17:56:58 +0900

    feat: too complex 확인

--- all-in-one/2nd/0.basic/2nd.ts
+++ all-in-one/2nd/0.basic/2nd.ts
@@ -1,6 +1,6 @@
 // 템플릿 리터럴 타입
-type World = "world" | "hell";
-const a: World = "world";
+type World = 'world' | 'hell';
+const a: World = 'world';
 
 const b = `hello ${a}`;
 
@@ -9,6 +9,11 @@ type Greeting = `hello ${World}`;
 const c: Greeting = 'hello world';
 const d: Greeting = 'hello hell';
 
-console.log(a, b, c, d)
+console.log(a, b, c, d);
 
-export {}
+export {};
+
+const root = document.querySelector(`#root`)!;
+
+type NumberRange = `${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0}`;
+type NumberRanges = `${NumberRange}${NumberRange}${NumberRange}${NumberRange}`;
