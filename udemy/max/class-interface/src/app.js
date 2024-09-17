1abf6ff05c962db13155aef564d058895539de54
raehan <raehan900809@gmail.com>
2024-09-17 22:48:44 +0900

    feat: 클래스로 싱글톤 생성한 것 확인

--- udemy/max/class-interface/src/app.js
+++ udemy/max/class-interface/src/app.js
@@ -1,4 +1,3 @@
-"use strict";
 class Department {
     name;
     constructor(name) {
@@ -34,3 +33,27 @@ const person1 = new Person1('Raehan');
 const person2 = new Person2('Max');
 person1.printName();
 person2.printName();
+class Person {
+    name;
+    static instance;
+    constructor(name) {
+        this.name = name;
+    }
+    static getInstance(name) {
+        if (Person.instance) {
+            return this.instance;
+        }
+        if (!name) {
+            throw new Error('No name provided');
+        }
+        this.instance = new Person(name);
+        return this.instance;
+    }
+    printName() {
+        console.log(this.name);
+    }
+}
+const personA = Person.getInstance('Raehan');
+const personB = Person.getInstance();
+console.log('personA is personB: ', personA === personB);
+export {};
