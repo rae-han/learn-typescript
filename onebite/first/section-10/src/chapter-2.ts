de73b110257bcd023f3a11c9037117fb4913f946
raehan <raehan900809@gmail.com>
2025-02-19 00:48:13 +0900

    feature: Omit 유틸리티 타입 구현

--- onebite/first/section-10/src/chapter-2.ts
+++ onebite/first/section-10/src/chapter-2.ts
@@ -19,3 +19,25 @@ const legacyPost: Pick<Post, 'title' | 'content'> = {
   title: '',
   content: '',
 };
+
+/**
+ * Omit<T, K>
+ * -> 뽑다, 고르다.
+ * -> 객체 프로퍼티로부터 특정 프로퍼티만 제거하는 타입.
+ */
+type Exclude<T, U> = T extends U ? never : T;
+
+type Omit<T, K extends keyof T> = {
+  [Key in keyof T as Key extends K ? never : Key]: T[Key];
+};
+
+// type Omit<T, K extends keyof T> = {
+//   [Key in keyof T extends K ? K : never]: T[Key];
+// };
+
+const noTitlePost: Omit<Post, 'title'> = {
+  title: '',
+  content: '',
+  tags: [] as string[],
+  thumbnailURL: '',
+};
