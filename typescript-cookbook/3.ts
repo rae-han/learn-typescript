af5d9dcad1cded44dd07c459c2252296503dfe7d
raehan <raehan900809@gmail.com>
2025-10-19 01:59:24 +0900

    docs: 실무로 통하는 타입스크립트 3장 내용 정리

--- typescript-cookbook/3.ts
+++ typescript-cookbook/3.ts
@@ -14,3 +14,18 @@ function handler(statusCode: number): boolean {
   return true;
 }
 fetchResults(handler);
+
+enum Traits {
+  None, // 0000
+  Friendly = 1, // 0001 또는 1 << 0
+  Mean = 1 << 1, // 0010
+  Funny = 1 << 2, // 0100
+  Boring = 1 << 3, // 1000
+}
+
+// (0010 | 0100) === 0110
+let aPersonsTraits = Traits.Mean | Traits.Funny;
+if ((aPersonsTraits & Traits.Mean) === Traits.Mean) {
+  console.log('Mean');
+  Traits.Mean;
+}
